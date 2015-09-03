115c026a0170e19d9e8f4960325e1d499950730c
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-09-03 20:33:27 +0200

    Some tables mapped

--- pasteque_admin/dummy/views.py
+++ pasteque_admin/dummy/views.py
@@ -2,7 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from wordpress_auth_lite.decorators import wordpress_login_required
+from wordpress_db.utils import get_pt_user_database
+
+from pasteque_db.database import Database, Credentials
+from pasteque_db.models import Application, CashRegister
+
 
 @wordpress_login_required
 def dummy(request):
-    return HttpResponse('<h1>hello world !</h1>')
+    pt_user_database = get_pt_user_database(request.wordpress_user)
+    credentials = Credentials(
+        driver='mysql',
+        host=pt_user_database.host,
+        port=pt_user_database.port,
+        user=pt_user_database.user,
+        password=pt_user_database.password,
+        name=pt_user_database.name
+    )
+    database = Database(credentials.generate_sqla_url())
+
+    session = database.new_session()
+
+    application = session.query(Application).filter_by(id='pasteque').one()
+    response = "<p>name: %s</p>" % application.name
+    response += "<p>version: %s</p>" % application.version
+
+    for loc in session.query(CashRegister).all():
+        response += "<p>%s</p>" % str(loc.location.name)
+
+    # response += "<p>Database = %s</p>" % database
+    # response = "<p>Credentials = %s</p>" % credentials
+
+    return HttpResponse(response)
--- pasteque_admin/pasteque_db/models.py
+++ pasteque_admin/pasteque_db/models.py
@@ -4,7 +4,7 @@
 # This is because of the dynamic nature (e.g per user) of the pasteque databases
 
 from sqlalchemy import Column, Date, DateTime, Float, ForeignKey, Index, Integer, String, Table, Text, text
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.dialects.mysql.base import BIT, MEDIUMBLOB
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -17,3 +17,59 @@ class Application(Base):
     id = Column('ID', String(255), primary_key=True)
     name = Column('NAME', String(255), nullable=False)
     version = Column('VERSION', String(255), nullable=False)
+
+class CashRegister(Base):
+    __tablename__ = 'CASHREGISTERS'
+
+    id = Column('ID', Integer, primary_key=True)
+    name = Column('NAME', String(255), nullable=False)
+    location_id = Column('LOCATION_ID', ForeignKey(u'LOCATIONS.ID'),
+        nullable=False, index=True)
+    next_ticket_id = Column('NEXTTICKETID', Integer,
+        nullable=False, server_default=text("'1'"))
+
+    location = relationship(u'Location', backref='cash_registers')
+
+class Location(Base):
+    __tablename__ = 'LOCATIONS'
+
+    id = Column('ID', String(255), primary_key=True)
+    name = Column('NAME', String(255), nullable=False, unique=True)
+    address = Column('ADDRESS', String(255))
+
+class Module(Base):
+    __tablename__ = 'MODULES'
+
+    user_id = Column('user_id', Integer, primary_key=True)
+    modules = Column('modules', Text, nullable=False)
+
+class PaymentMode(Base):
+    __tablename__ = 'PAYMENTMODES'
+
+    id = Column('ID', Integer, primary_key=True)
+    code = Column('CODE', String(255), nullable=False)
+    name = Column('NAME', String(255), nullable=False)
+    backname = Column('BACKNAME', String(255), nullable=False,
+        server_default=text("''"))
+    flags = Column('FLAGS', Integer, nullable=False,
+        server_default=text("'0'"))
+    active = Column('ACTIVE', BIT(1), nullable=False)
+    system = Column('SYSTEM', BIT(1), nullable=False)
+    disporder = Column('DISPORDER', Integer, nullable=False,
+        server_default=text("'0'"))
+    image = Column('IMAGE', MEDIUMBLOB)
+
+class PaymentModeReturn(Base):
+    __tablename__ = 'PAYMENTMODES_RETURNS'
+
+    payment_mode_id = Column('PAYMENTMODE_ID', ForeignKey(u'PAYMENTMODES.ID'),
+        primary_key=True, nullable=False)
+    min_price = Column('MIN', Float(asdecimal=True), primary_key=True,
+        nullable=False, server_default=text("'0'"))
+    return_mode_id = Column('RETURNMODE_ID',
+        ForeignKey(u'PAYMENTMODES.ID'), index=True)
+
+    payment_mode = relationship(u'PAYMENTMODE',
+        primaryjoin=lambda: PaymentModeReturn.payment_mode_id == PaymentMode.id)
+    return_mode = relationship(u'PAYMENTMODE',
+        primaryjoin=lambda: PaymentModeReturn.return_mode_id == PaymentMode.id)
